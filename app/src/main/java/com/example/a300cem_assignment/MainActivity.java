fe6216d0eb87008d14d69e08e978672967017b89
Mak Wing Hong <ansonmak3143@gmail.com>
2020-04-17 02:30:41 +0800

    set up the bottom bar and test to show something
    Signed-off-by: Mak Wing Hong <ansonmak3143@gmail.com>

--- app/src/main/java/com/example/a300cem_assignment/MainActivity.java
+++ app/src/main/java/com/example/a300cem_assignment/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.a300cem_assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(UserFragment.newInstance("", ""));
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_order:
+                            openFragment(OrderFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_pickOrder:
+                            openFragment(PickOrderFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_viewOrder:
+                            openFragment(ViewOrderFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_user:
+                            openFragment(UserFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
