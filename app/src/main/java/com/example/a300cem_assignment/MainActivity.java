394d05a04e03e06794e6d9f782982a36e80d595c
Mak Wing Hong <ansonmak3143@gmail.com>
2020-05-08 19:45:25 +0800

    update MainActivity class set up the register class in MainActivity class set up user SharedPreferences
    Signed-off-by: Mak Wing Hong <ansonmak3143@gmail.com>

--- app/src/main/java/com/example/a300cem_assignment/MainActivity.java
+++ app/src/main/java/com/example/a300cem_assignment/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.a300cem_assignment;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -12,32 +15,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigation;
+    private SharedPreferences sharedPrefs;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        openFragment(UserFragment.newInstance("", ""));
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        sharedPrefs = getSharedPreferences("userInfor", MODE_PRIVATE);
+        if(!sharedPrefs.contains("initialized")){
+            openFragment(RegisterFragment.newInstance());
+        }else{
+            String tmpUserName, tmpUserPhone;
+            tmpUserName = sharedPrefs.getString("userName","Error");
+            tmpUserPhone = sharedPrefs.getString("userPhone","999");
+            bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+            openFragment(OrderFragment.newInstance(tmpUserName, tmpUserPhone));
+        }
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    String tmpUserName, tmpUserPhone;
+                    tmpUserName = sharedPrefs.getString("userName","Error");
+                    tmpUserPhone = sharedPrefs.getString("userPhone","999");
                     switch (item.getItemId()) {
                         case R.id.navigation_order:
-                            openFragment(OrderFragment.newInstance("", ""));
+                            openFragment(OrderFragment.newInstance(tmpUserName, tmpUserPhone));
                             return true;
                         case R.id.navigation_pickOrder:
-                            openFragment(PickOrderFragment.newInstance("", ""));
+                            openFragment(PickOrderFragment.newInstance());
                             return true;
                         case R.id.navigation_viewOrder:
-                            openFragment(ViewOrderFragment.newInstance("", ""));
+                            openFragment(ViewOrderFragment.newInstance());
                             return true;
                         case R.id.navigation_user:
-                            openFragment(UserFragment.newInstance("", ""));
+                            openFragment(UserFragment.newInstance(tmpUserName, tmpUserPhone));
                             return true;
                     }
                     return false;
