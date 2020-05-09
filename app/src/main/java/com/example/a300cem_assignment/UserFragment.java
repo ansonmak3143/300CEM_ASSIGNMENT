26ff7082c96476b9e604bc6cd9a6b80115dea4e4
Mak Wing Hong <ansonmak3143@gmail.com>
2020-05-10 02:46:28 +0800

    bug fix and implement the function
    Signed-off-by: Mak Wing Hong <ansonmak3143@gmail.com>

--- app/src/main/java/com/example/a300cem_assignment/UserFragment.java
+++ app/src/main/java/com/example/a300cem_assignment/UserFragment.java
@@ -1,35 +1,34 @@
 package com.example.a300cem_assignment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class UserFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class UserFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_PARAM1 = "userName";
+    private static final String ARG_PARAM2 = "userPhone";
+    private String userName, userPhone;
+    private EditText userEditName, userEditPhone;
+    private TextView userNameTxt, userPhoneTxt;
+    private LinearLayout userEditInforLayout;
+    private SharedPreferences sharedPrefs;
+    private  Button submit,cancel,edit;
+
 
     public UserFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
+    static UserFragment newInstance(String param1, String param2) {
         UserFragment fragment = new UserFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -42,15 +41,77 @@ public class UserFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            userName = getArguments().getString(ARG_PARAM1);
+            userPhone = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sharedPrefs = getActivity().getSharedPreferences("userInfor", Context.MODE_PRIVATE);
+        userNameTxt = view.findViewById(R.id.userNameTxt);
+        userPhoneTxt = view.findViewById(R.id.userPhoneTxt);
+        userEditName = view.findViewById(R.id.userInputEditName);
+        userEditPhone = view.findViewById(R.id.userInputEditPhone);
+        submit = view.findViewById(R.id.userEditInforSubmitBtn);
+        cancel = view.findViewById(R.id.userInforCancel);
+        edit = view.findViewById(R.id.userInforEdit);
+        userEditInforLayout = view.findViewById(R.id.userEditInforLayout);
+        userNameTxt.setText(userName);
+        userPhoneTxt.setText(userPhone);
+        submit.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        edit.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.userEditInforSubmitBtn:
+                SharedPreferences.Editor editer = sharedPrefs.edit();
+                String tmpUserName, tmpUserPhone;
+                tmpUserName = userEditName.getText().toString().trim();
+                tmpUserPhone = userEditPhone.getText().toString().trim();
+                if (isEmptyString(tmpUserName)) {
+                    Toast.makeText(getActivity(), getString(R.string.toastEmptyName), Toast.LENGTH_SHORT).show();
+                } else if (isEmptyString(tmpUserPhone)) {
+                    Toast.makeText(getActivity(), getString(R.string.toastEmptyPhone), Toast.LENGTH_SHORT).show();
+                } else {
+                    editer.putString("userName", tmpUserName);
+                    editer.putString("userPhone", tmpUserPhone);
+                    editer.putBoolean("initialized", true);
+                    editer.apply();
+                    userNameTxt.setText(tmpUserName);
+                    userPhoneTxt.setText(tmpUserPhone);
+                    cancel.setVisibility(View.GONE);
+                    submit.setVisibility(View.GONE);
+                    userEditInforLayout.setVisibility(View.GONE);
+                    edit.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.userInforCancel:
+                cancel.setVisibility(View.GONE);
+                submit.setVisibility(View.GONE);
+                userEditInforLayout.setVisibility(View.GONE);
+                edit.setVisibility(View.VISIBLE);
+                break;
+            case R.id.userInforEdit:
+                edit.setVisibility(View.GONE);
+                userEditInforLayout.setVisibility(View.VISIBLE);
+                cancel.setVisibility(View.VISIBLE);
+                submit.setVisibility(View.VISIBLE);
+                break;
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    private static boolean isEmptyString(String text) {
+        return (text == null || text.trim().equals("null") || text.trim()
+                .length() <= 0);
     }
 }
