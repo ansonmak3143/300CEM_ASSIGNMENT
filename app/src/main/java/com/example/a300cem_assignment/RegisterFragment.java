4ca139bceca1afae5f30d34ce86737a6b4fc62d7
Mak Wing Hong <ansonmak3143@gmail.com>
2020-05-10 02:46:05 +0800

    bug fix and implement the function
    Signed-off-by: Mak Wing Hong <ansonmak3143@gmail.com>

--- app/src/main/java/com/example/a300cem_assignment/RegisterFragment.java
+++ app/src/main/java/com/example/a300cem_assignment/RegisterFragment.java
@@ -2,13 +2,9 @@ package com.example.a300cem_assignment;
 
         import android.content.Context;
         import android.content.SharedPreferences;
-        import android.os.Build;
         import android.os.Bundle;
-
-        import androidx.annotation.RequiresApi;
         import androidx.fragment.app.Fragment;
         import androidx.fragment.app.FragmentTransaction;
-
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
@@ -16,17 +12,14 @@ package com.example.a300cem_assignment;
         import android.widget.EditText;
         import android.widget.Toast;
 
-        import java.util.Objects;
-
-@RequiresApi(api = Build.VERSION_CODES.KITKAT)
 public class RegisterFragment extends Fragment implements View.OnClickListener{
 
-    private SharedPreferences sharedPrefs = Objects.requireNonNull(getActivity()).getSharedPreferences("userInfor", Context.MODE_PRIVATE);
-    private Button regBtn = getActivity().findViewById(R.id.registerSubmit);
-    private EditText userName = getActivity().findViewById(R.id.userInputRegisterName), userPhone = getActivity().findViewById(R.id.userInputRegisterPhone);
+    private SharedPreferences sharedPrefs;
+    private Button regBtn;
+    private EditText userName;
+    private EditText userPhone;
 
     public RegisterFragment() {
-        // Required empty public constructor
     }
 
     static RegisterFragment newInstance() {
@@ -36,6 +29,15 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sharedPrefs = getActivity().getSharedPreferences("userInfor", Context.MODE_PRIVATE);
+        regBtn = view.findViewById(R.id.registerSubmit);
+        userName = view.findViewById(R.id.userInputRegisterName);
+        userPhone = view.findViewById(R.id.userInputRegisterPhone);
         regBtn.setOnClickListener(this);
     }
 
@@ -65,12 +67,11 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     private void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
